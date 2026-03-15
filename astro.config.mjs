e43387d608b01953a07afdd79893b69341607dd2
bullemer <bullemer@maritimedatasystems.com>
2026-03-15 19:55:04 +0100

    feat: implement comprehensive SEO descriptions, canonicals, and open graph meta tags across all pages; hotfix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,8 +10,5 @@ export default defineConfig({
         markdoc(),
         keystatic()
     ],
-    output: 'static',
-    adapter: node({
-        mode: 'standalone'
-    })
+    output: 'static'
 });
